cf23a7b5b7bd7f44ecbe8ab197c9ada7cb14dcf6
Rhys <rhys26@hotmail.com>
2021-06-04 18:24:12 +0100

    initial commit

---
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Movement
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        public GameObject Player;
+        public float MovementMultiplier = 0.5f;
+
+        void Start()
+        {
+
+        }
+
+        void FixedUpdate()
+        {
+            var horizontalMove = Input.GetAxisRaw("Horizontal") * MovementMultiplier;
+            var verticalMove = Input.GetAxisRaw("Vertical") * MovementMultiplier;
+
+            Player.transform.position = new Vector3(Player.transform.position.x + horizontalMove, Player.transform.position.y + verticalMove);
+        }
+    }
+}
