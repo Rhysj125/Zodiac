1581401ed248eb66af322a704577b4c17dcb46da
Rhys <rhys26@hotmail.com>
2021-06-04 21:54:41 +0100

    Adding the character to the scene. Character changes direction depending on direction of travel. Character hitbox correctly follows when flipping on the x-axis

--- Assets/Scripts/Movement/CharacterController.cs
+++ Assets/Scripts/Movement/CharacterController.cs
@@ -4,29 +4,51 @@ namespace Assets.Scripts.Movement
 {
     public class CharacterController : MonoBehaviour
     {
+        // Settings
         [Range(.5f, 10.0f)]
         public float MovementSpeed = 5.0f;
         [Range(.5f, 10.0f)]
         public float JumpForce = 5.0f;
         public float InAirMovementMulitplier = 0.2f;
 
+        // Components
         private Rigidbody2D _rigidBody;
+        private SpriteRenderer _spriteRenderer;
+        private BoxCollider2D _boxCollider;
+
+        // States
         private bool _isJumping = false;
         private bool _isGrounded = true;
 
+        // Movement Vectors
         private float _horizontalMovement = 0f;
         private float _jumpingHozitonalVector = 0f;
 
         void Awake()
         {
             _rigidBody = GetComponent<Rigidbody2D>();
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            _boxCollider = GetComponent<BoxCollider2D>();
         }
 
+        // Use for inputs
         void Update()
         {
             _isGrounded = Mathf.Abs(_rigidBody.velocity.y) < 0.001;
 
             _horizontalMovement = Input.GetAxisRaw("Horizontal");
+
+            if(_horizontalMovement > 0)
+            {
+                // Deal with collider too left x offset = -0.12, right x offset = .12
+                _boxCollider.offset = new Vector2(0.12f, 0);
+                _spriteRenderer.flipX = true;
+            }
+            else if(_horizontalMovement < 0)
+            {
+                _boxCollider.offset = new Vector2(-0.12f, 0);
+                _spriteRenderer.flipX = false;
+            }
 
             if (Input.GetButtonDown("Jump"))
             {
@@ -34,6 +56,7 @@ namespace Assets.Scripts.Movement
             }
         }
 
+        // Use mostly for actual movement
         void FixedUpdate()
         {
             if (!_isGrounded)
