ffbde5b7589f2dcb771f627a49630e5bfbf8999f
Rhys <rhys26@hotmail.com>
2021-06-06 15:29:02 +0100

    Creating menu's, Allowing for pausing and rejigged movement (still needs work)

---
+++ Assets/Scripts/Menu/PauseMenuScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.Menu
+{
+    public class PauseMenuScript : MonoBehaviour
+    {
+        public void ReturnToMainMenu()
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+
+        public void Exit()
+        {
+            Application.Quit();
+        }
+    }
+}
