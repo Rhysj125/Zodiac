7e272c1c0dd38cb575b99deb312dcc45eb1d1426
Rhys <rhys26@hotmail.com>
2021-06-10 18:15:54 +0100

    Adding the fish into the scene and it slowly moves across

---
+++ Assets/Scripts/Enemies/PiscesMovementScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Enemies
+{
+    public class PiscesMovementScript : MonoBehaviour
+    {
+
+        // Internal components
+        private Rigidbody2D _rigidBody;
+
+        void Awake()
+        {
+            _rigidBody = GetComponent<Rigidbody2D>();
+        }
+
+        void FixedUpdate()
+        {
+            _rigidBody.velocity = new Vector2(-.9f, 0);
+        }
+    }
+}
