342fc460746c2008f5881682427e02c6089db35c
Rhys <rhys26@hotmail.com>
2021-06-12 10:47:44 +0100

    A wild flailing fish really wants to eat you..Shame I've not added that bit yet. Re jigged files again because things are in places they shouldn't

--- Assets/Scripts/Enemies/PiscesMovementScript.cs
+++ Assets/Scripts/Enemies/PiscesMovementScript.cs
@@ -4,18 +4,59 @@ namespace Assets.Scripts.Enemies
 {
     public class PiscesMovementScript : MonoBehaviour
     {
+        // External components
+        public GameObject Player;
 
         // Internal components
         private Rigidbody2D _rigidBody;
 
-        void Awake()
+        private bool _attacking = false;
+        private float _attackStartingYPosition = -99999;
+
+        private void Awake()
         {
             _rigidBody = GetComponent<Rigidbody2D>();
         }
 
-        void FixedUpdate()
+        private void FixedUpdate()
         {
-            _rigidBody.velocity = new Vector2(-.9f, 0);
+            var rng = Random.value;
+
+            if (!_attacking && rng > 0.95)
+            {
+                _attacking = true;
+                InitiateJumpOverAttack();
+            }
+
+            if (_attacking)
+            {
+                if(gameObject.transform.position.y < _attackStartingYPosition)
+                {
+                    _attacking = false;
+                }
+            }
+
+            if (!_attacking)
+            {
+                _rigidBody.mass = 0;
+                _rigidBody.gravityScale = 0;
+                _rigidBody.velocity = new Vector2(-0.9f, 0);
+            }
+        }
+
+        private void InitiateJumpOverAttack()
+        {
+            _rigidBody.velocity = new Vector2(0, 0);
+            _rigidBody.mass = 1;
+            _rigidBody.gravityScale = 1;
+
+            _attackStartingYPosition = gameObject.transform.position.y - 1;
+
+            var playerXPosition = Player.transform.position.x;
+
+            gameObject.transform.position = new Vector3(playerXPosition, gameObject.transform.position.y);
+
+            _rigidBody.AddForce(new Vector2(0, 15), ForceMode2D.Impulse);
         }
     }
 }
