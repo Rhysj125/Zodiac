dd90fb2b87271d7fa9b310b890373bdb38bb1c26
Rhys <rhys26@hotmail.com>
2021-06-14 16:25:02 +0100

    Removing polutants from the water

--- Assets/Scripts/Water/WaterScript.cs
+++ Assets/Scripts/Water/WaterScript.cs
@@ -5,15 +5,6 @@ namespace Assets.Scripts.Water
 {
     public class WaterScript : MonoBehaviour
     {
-        // Components
-        private BoxCollider2D _boxCollider;
-        public GameObject _player;
-
-        void Awake()
-        {
-            _boxCollider = GetComponent<BoxCollider2D>();
-        }
-
         void OnTriggerEnter2D(Collider2D other)
         {
             if (other.tag == "Player")
